c0c02d1ebf83754024a7dfff947ebc569be7cf6a
JonathanPucci <jonathanpucci06@gmail.com>
2018-01-20 19:25:34 +0100

    fix

--- tables.sql
+++ tables.sql
@@ -12,8 +12,6 @@ CREATE TABLE "Users" (
 
 CREATE TABLE "Events" (
 	"Event_ID" serial NOT NULL,
-	"Location_longitude" serial NOT NULL,
-	"Location_latitude" serial NOT NULL,
 	"Description" TEXT,
 	"Photo" TEXT,
 	"Date" DATE NOT NULL,
@@ -21,7 +19,6 @@ CREATE TABLE "Events" (
 	"Spot_ID" bigint NOT NULL,
 	"Participants_min" int NOT NULL,
 	"Participants_max" int NOT NULL,
-	"Participants_number" int NOT NULL,
 	"Sport" TEXT NOT NULL,
 	CONSTRAINT Events_pk PRIMARY KEY ("Event_ID")
 ) WITH (
@@ -34,7 +31,6 @@ CREATE TABLE "Spots" (
 	"Spot_ID" serial NOT NULL,
 	"Spot_longitude" bigint NOT NULL,
 	"Spot_latitude" bigint NOT NULL,
-	"Fields" TEXT NOT NULL,
 	CONSTRAINT Spots_pk PRIMARY KEY ("Spot_ID")
 ) WITH (
   OIDS=FALSE
